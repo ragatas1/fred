6b455585a040eb9fb011dc09233216b96f281a2f
Sudozilla <Vebjorn.alvaer@gmail.com>
2024-04-09 10:09:06 +0200

    AI waypoint movement?

---
+++ spill syltetoy fred/Assets/Script/ForelderAI.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForelderAI : MonoBehaviour
+{
+    public List<Transform> waypoints;
+    Transform currentTarget;
+    private int _index = 1;
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+
+    private bool _inReverse = false;
+    private bool _atEnd = false;
+    private bool _movving = true;
+
+    //https://bergstrand-niklas.medium.com/simple-waypoint-system-in-unity-f3ef3665d636
+    //linken jeg brukte^^
+
+    void Start()
+    {
+       _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+
+        if (_agent != null)
+            Debug.LogError("NavMeshAgent missing");
+
+        if (_animator != null)
+            Debug.LogError("Animator Missing");
+
+        //If there are waypoints and the first waypoint is not null
+        if (waypoints.Count > 0 && waypoints[0] != null)
+        {
+            //Set first target
+            currentTarget = waypoints[_index];
+
+            //Start moving the ai(agent) towards the first target
+            _agent.SetDestination(currentTarget.position);
+        }
+    }
+
+    IEnumerator MoveToNextWaypoint()
+    { 
+        if (!_inReverse)
+        {
+            _index++;
+        }
+
+        //If not at the last waypoint and not going in reverse
+        if (_index < waypoints.Count && !_inReverse)
+        {
+            //Pause a random amount of time before going to the first point
+            if (_index == 1)
+                yield return new WaitForSeconds(Random.Range(3f, 6f));
+
+            currentTarget = waypoints[_index];
+        }
+        else
+        {
+            //If at the last point pause for a random amount of time
+            if (!_atEnd)
+            {
+                _atEnd = true;
+                yield return new WaitForSeconds(Random.Range(3f, 6f));
+            }
+
+            _index--;
+            _inReverse = true;
+
+
+            //if next waypoint is the first waypoint reset _inReverse and _atEnd
+            if (_index == 0)
+            {
+                _inReverse = false;
+                _atEnd = false;
+            }
+
+            currentTarget = waypoints[_index];
+        }
+
+        //Move AI (agent) to next waypoint
+        _agent.SetDestination(currentTarget.position);
+        _moving = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}

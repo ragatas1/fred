2608652cbb03069b2f78f7474b07d73d21522aac
ragatas1 <emil.sonstaberg@gmail.com>
2024-04-23 13:26:49 +0200

    mgs

--- spill syltetoy fred/Assets/Script/DorApneScript.cs
+++ spill syltetoy fred/Assets/Script/DorApneScript.cs
@@ -2,12 +2,14 @@ using BrokenVector.LowPolyFencePack;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DorApneScript : MonoBehaviour
 {
     public DoorController door;
     public SpillerPickup spiller;
     bool vedDor;
+    public BoxCollider box;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,14 @@ public class DorApneScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (vedDor && spiller.HarGameboy)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                door.OpenDoor();
+                box.enabled = false;
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {

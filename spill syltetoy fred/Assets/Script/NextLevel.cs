d6ef42550d52d5ae3162ce396302c1ea49f52d72
ragatas1 <emil.sonstaberg@gmail.com>
2024-04-25 12:54:39 +0200

    funka nå

--- spill syltetoy fred/Assets/Script/NextLevel.cs
+++ spill syltetoy fred/Assets/Script/NextLevel.cs
@@ -6,27 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    public GameObject falseProphet;
-
-    void OnLevelWasLoaded(Scene scene, LoadSceneMode mode)
-    {
-        falseProphet = GameObject.FindGameObjectWithTag("logikk");
-        Destroy(falseProphet);
-    }
-    private void OnLevelWasLoaded()
-    {
-        falseProphet = GameObject.FindGameObjectWithTag("logikk");
-        Destroy(falseProphet);
-    }
+    public string forigeScene;
+    public string denneScenen;
+    public static NextLevel instance;
+    public List <string> scener;
     private void Start()
     {
-        SceneManager.sceneLoaded += OnLevelLoaded;
-        StartCoroutine(starten());
-    }
-    IEnumerator starten()
-    {
-        yield return new WaitForSeconds(0.1f);
-        gameObject.tag = "logikkTrue";
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
         DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            sjekkScene();
+        }
+    }
+    public void sjekkScene()
+    {
+        if (denneScenen != null)
+        {
+            forigeScene = denneScenen;
+        }
+        Scene currentScene = SceneManager.GetActiveScene();
+        denneScenen = currentScene.name;
     }
 }

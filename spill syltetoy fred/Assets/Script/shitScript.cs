b79c28028b36e9396e8273a433f1c53ff8efae05
ragatas1 <emil.sonstaberg@gmail.com>
2024-04-26 21:40:51 +0200

    fiksa problem med at banen spilte igjen kanskje

--- spill syltetoy fred/Assets/Script/shitScript.cs
+++ spill syltetoy fred/Assets/Script/shitScript.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class shitScript : MonoBehaviour
 {
+    NextLevel nextLevel;
+    public GameObject test;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = GameObject.FindGameObjectWithTag("logikk");
+        nextLevel = test.GetComponent<NextLevel>();
+        nextLevel.sjekkScene();
     }
 
     // Update is called once per frame
